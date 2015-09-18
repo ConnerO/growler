6a2f74842f0a7fc6d2bed475f599c5ca1223daf6
Adam Weitzman <adamcweitzman@gmail.com>
2015-09-18 11:47:44 -0500

    buttonadded

--- starter/app.rb
+++ starter/app.rb
@@ -13,9 +13,22 @@ get '/' do
 	erb :index
 end
 
+get '/wall' do
+
+	@growl_array = session[:growl_array]
+
+	erb :wall
+end
+
+
+
+
 post '/wall' do
 
-	# session[:new_growl] << params[:new_growl]
+
+	
+
+	session[:growl_array] << params[:new_growl]
 		
 	@new_growl = params[:new_growl]
 
